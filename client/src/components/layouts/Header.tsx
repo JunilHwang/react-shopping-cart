fd029f8863128398865744104cf88830ca325778
junilhwang <junil.h@kakao.com>
2023-01-29 01:24:12 +0900

    feat: Header 컴포넌트 추가

---
+++ client/src/components/layouts/Header.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav flex justify-around">
+        <div className="flex-center">
+          <Link to="/">
+            <h3 className="nav-title">CLEAN CODE SHOP</h3>
+          </Link>
+        </div>
+        <div className="flex items-center gap-15">
+          <Link to="/cart" className="nav-button">
+            장바구니
+          </Link>
+          <Link to="/orderList" className="nav-button">
+            주문목록
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
